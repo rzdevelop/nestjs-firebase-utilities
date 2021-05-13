9609c16db10c4e8a0acac95fc8cc8070be018b81
Renan Zelaya <renan_zelaya@hotmail.com>
2021-05-12 22:32:18 -0600

    add async support to register module

--- src/common/auth/firebase/firebase.interface.ts
+++ src/common/auth/firebase/firebase.interface.ts
@@ -1,3 +1,4 @@
+import { ModuleMetadata } from '@nestjs/common/interfaces';
 import { AppOptions, ServiceAccount, auth } from 'firebase-admin';
 import { JwtFromRequestFunction } from 'passport-jwt';
 import { CustomLogger } from 'nestjs-utilities';
@@ -11,3 +12,11 @@ export interface FirebaseAuthStrategyOptions {
 export type FirebaseDecodedToken = auth.DecodedIdToken;
 export type FirebaseAppOptions = AppOptions;
 export type FirebaseServiceAccount = ServiceAccount;
+
+export interface FirebaseModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+  name?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  useFactory?: (...args: any[]) => Promise<FirebaseAppOptions> | FirebaseAppOptions;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  inject?: any[];
+}
--- src/common/auth/firebase/firebase.module.ts
+++ src/common/auth/firebase/firebase.module.ts
@@ -1,6 +1,7 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { CustomLoggerModule, CustomLogger } from 'nestjs-utilities';
+import { FirebaseAppOptions, FirebaseModuleAsyncOptions } from './firebase.interface';
 import { FirebaseService, IFirebaseOptions } from './firebase.service';
 
 @Module({})
@@ -17,6 +18,33 @@ export class FirebaseModule {
         },
       ],
       exports: [FirebaseService],
+    };
+  }
+
+  static registerAsync(options: FirebaseModuleAsyncOptions): DynamicModule {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const defaultFn = (): void => {};
+    const optionsProvider: Provider = {
+      provide: 'FirebaseModuleAsyncOptions',
+      useFactory: options.useFactory || defaultFn,
+      inject: options.inject || [],
+    };
+
+    return {
+      module: FirebaseModule,
+      imports: [PassportModule, CustomLoggerModule.register(), ...(options.imports || [])],
+      providers: [
+        optionsProvider,
+        {
+          provide: FirebaseService,
+          useFactory: (logger: CustomLogger, opts: FirebaseAppOptions): FirebaseService => {
+            logger.info('options', opts);
+            return new FirebaseService(logger, { getOptions: (): FirebaseAppOptions => opts });
+          },
+          inject: [CustomLogger, 'FirebaseModuleAsyncOptions'],
+        },
+      ],
+      exports: [optionsProvider, FirebaseService],
     };
   }
 }
